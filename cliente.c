3600723ea1a74434fcc2998a632837245534eb22
pedrosaaaaa <pedro75889@gmail.com>
2026-02-28 00:40:39 +0100

    -

--- cliente.c
+++ cliente.c
@@ -1,9 +1,14 @@
-// cliente.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <termios.h>
+#include <string.h>
+
+typedef struct {
+    uint8_t key;
+    uint8_t pressed;
+} InputPacket;
 
 void enable_raw_mode() {
     struct termios t;
@@ -14,23 +19,31 @@ void enable_raw_mode() {
 
 int main() {
     int sock;
-    struct sockaddr_in server;
+    struct sockaddr_in server_addr;
+    InputPacket packet;
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
 
-    server.sin_family = AF_INET;
-    server.sin_port = htons(6000);
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    connect(sock, (struct sockaddr*)&server, sizeof(server));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(6000);
+    inet_pton(AF_INET, "10.20.33.103", &server_addr.sin_addr);
 
     enable_raw_mode();
 
+    printf("Enviando inputs por UDP...\n");
+
     char c;
     while (read(STDIN_FILENO, &c, 1) > 0) {
-        send(sock, &c, 1, 0);
+        packet.key = c;
+        packet.pressed = 1;
+        sendto(sock, &packet, sizeof(packet), 0,
+               (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+        // enviar key release inmediatamente
+        packet.pressed = 0;
+        sendto(sock, &packet, sizeof(packet), 0,
+               (struct sockaddr*)&server_addr, sizeof(server_addr));
     }
 
-    close(sock);
     return 0;
 }
--- servidor.c
+++ servidor.c
@@ -1,32 +1,88 @@
-// servidor.c
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <linux/uinput.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+
+typedef struct {
+    uint8_t key;
+    uint8_t pressed;
+} InputPacket;
+
+int create_uinput() {
+    int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+
+    ioctl(fd, UI_SET_EVBIT, EV_KEY);
+    ioctl(fd, UI_SET_KEYBIT, KEY_W);
+    ioctl(fd, UI_SET_KEYBIT, KEY_A);
+    ioctl(fd, UI_SET_KEYBIT, KEY_S);
+    ioctl(fd, UI_SET_KEYBIT, KEY_D);
+
+    struct uinput_setup usetup;
+    memset(&usetup, 0, sizeof(usetup));
+    usetup.id.bustype = BUS_USB;
+    strcpy(usetup.name, "doom-virtual-keyboard");
+
+    ioctl(fd, UI_DEV_SETUP, &usetup);
+    ioctl(fd, UI_DEV_CREATE);
+
+    sleep(1);
+    return fd;
+}
+
+void send_key(int fd, int keycode, int pressed) {
+    struct input_event ev;
+
+    memset(&ev, 0, sizeof(ev));
+    ev.type = EV_KEY;
+    ev.code = keycode;
+    ev.value = pressed;
+    write(fd, &ev, sizeof(ev));
+
+    ev.type = EV_SYN;
+    ev.code = SYN_REPORT;
+    ev.value = 0;
+    write(fd, &ev, sizeof(ev));
+}
+
+int map_key(uint8_t key) {
+    switch (key) {
+        case 'w': return KEY_W;
+        case 'a': return KEY_A;
+        case 's': return KEY_S;
+        case 'd': return KEY_D;
+        default: return -1;
+    }
+}
 
 int main() {
-    int server_fd, client_fd;
-    struct sockaddr_in address;
-    char buffer[1];
+    int sock;
+    struct sockaddr_in server_addr;
+    InputPacket packet;
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int uinput_fd = create_uinput();
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(6000);
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
 
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    listen(server_fd, 1);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(6000);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    printf("Esperando cliente...\n");
-    client_fd = accept(server_fd, NULL, NULL);
-    printf("Cliente conectado\n");
+    bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
-    while (recv(client_fd, buffer, 1, 0) > 0) {
-        printf("Tecla recibida: %c\n", buffer[0]);
+    printf("Servidor UDP escuchando en puerto 6000...\n");
+
+    while (1) {
+        recv(sock, &packet, sizeof(packet), 0);
+
+        int keycode = map_key(packet.key);
+        if (keycode != -1) {
+            send_key(uinput_fd, keycode, packet.pressed);
+        }
     }
 
-    close(client_fd);
-    close(server_fd);
     return 0;
 }
