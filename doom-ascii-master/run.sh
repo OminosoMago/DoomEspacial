5bf1363e3878a3062d38f1104362be54bb40be30
pedrosaaaaa <pedro75889@gmail.com>
2026-03-01 00:15:31 +0100

    Update run.sh

--- doom-ascii-master/run.sh
+++ doom-ascii-master/run.sh
@@ -7,15 +7,18 @@ read -p "Escriba su respuesta" opcion
 if [ $opcion -eq 1 ]; then
 	echo "escogiste la opcion 1"
 	#ttyd --writable -p 8080 ./doom-ascii -iwad DOOM1.WAD -chars block
-	./_unix/game/doom-ascii -iwad DOOM1.WAD -chars block
+	cd _unix/game
+	./doom-ascii -iwad /../../DOOM1.WAD -chars block
 elif [ $opcion -eq 2 ]; then
 	echo "escogiste la opcion 2"
 	#ttyd --writable -p 8080 ../doom-ascii -iwad DOOM1.WAD -chars ascii
-	./_unix/game/doom-ascii -iwad DOOM1.WAD -chars ascii
+	cd _unix/game
+	./doom-ascii -iwad /../../DOOM1.WAD -chars ascii
 elif [ $opcion -eq 3 ]; then
 	echo "escogiste la opcion 3"
 	#ttyd --writable -p 8080 ./doom-ascii -iwad DOOM1.WAD -chars ascii -nocolor
-	./_unix/game/doom-ascii -iwad DOOM1.WAD -chars ascii -nocolor
+	cd _unix/game
+	./doom-ascii -iwad /../../DOOM1.WAD -chars ascii -nocolor
 else
 	echo "No entiendo nada adios"
 fi
